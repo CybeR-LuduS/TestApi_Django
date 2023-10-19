7cc5e8c5afa427eb5d4684f06a7f2d7fff5d54d0
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-10-19 10:22:39 -0300

    Modelos Vehiculo y Usuario

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from core.models import Categoria
+from core.models import Vehiculo, Categoria, Usuario
+
+
+from rest_framework import serializers
+from core.models import Vehiculo
+
+class VehiculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehiculo
+        fields = ['patente', 'marca', 'modelo', 'año', 'color', 'capacidad_pasajeros', 'capacidad_carga', 'tipo_combustible', 'kilometraje', 'estado_mantenimiento', 'fecha_compra', 'propietario']
+
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ['id', 'nombre', 'descripcion']
+
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['rut', 'nombre', 'apellidoPaterno', 'apellidoMaterno', 'fechaNacimiento', 'carrera', 'sede', 'idCategoria', 'isActive', 'patenteVehiculo']
