d7fb662b7685610d75dcd71d3effb7e73e331874
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-11-02 19:42:42 -0300

    Creación y migración modelo Viaje

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Vehiculo, Categoria, Usuario
+from .models import Vehiculo, Categoria, Usuario, Viaje
 
 # Register your models here.
 
 admin.site.register(Vehiculo)
 admin.site.register(Categoria)
 admin.site.register(Usuario)
+admin.site.register(Viaje)
